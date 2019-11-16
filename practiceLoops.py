cb39c0f1bce76da226f563610b2f83a7ce52fa8e
ldiarra25 <55326296+ldiarra25@users.noreply.github.com>
2019-11-16 08:11:19 -0600

    Initial commit

---
+++ practiceLoops.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 2, 2019
+@author: ITAUser
+'''
+fruits = ["apple", "blueberry", "raspberry"]
+for fruit in fruits:
+    print(fruit)
+
+
+n = 5
+for i in range(n):
+    for j in range(i):
+        print("* ", end = " ")
+    print("  ,m .,")    
